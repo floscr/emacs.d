2104a8de4f8a6e3b16287bb64750800f02ba8f71
Florian Schroedl <flo.schroedl@gmail.com>
2023-10-17 20:37:41 +0200

    Add babashka main file code

--- project-templates/babashka/src/__PROJECT-NAME__/core.clj
+++ project-templates/babashka/src/__PROJECT-NAME__/core.clj
@@ -1,3 +1,15 @@
 #!/usr/bin/env bb
 (ns __PROJECT-NAME__.core
   (:require))
+
+;; Main ------------------------------------------------------------------------
+
+(defn -main [& args]
+  (println args))
+
+(when (= *file* (System/getProperty "babashka.file"))
+  (apply -main *command-line-args*))
+
+(comment
+  (-main)
+  nil)
