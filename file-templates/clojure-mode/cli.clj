0e1bfa42e8aa2d590313ba932efc99e6621424b0
Florian Schroedl <flo.schroedl@gmail.com>
2023-11-23 15:05:48 +0100

    Add cli file template

---
+++ file-templates/clojure-mode/cli.clj
@@ -0,0 +1,24 @@
+# -*- mode: snippet -*-
+# name: Clojure Cli Template
+# group: file templates
+# key: __cli
+# --
+(ns `(my-clojure/current-namespace)`
+  (:require
+   [babashka.cli :as cli]))
+
+;; Commands --------------------------------------------------------------------
+
+(defn main [opts]
+  (println opts "Hello world"))
+
+;; Main ------------------------------------------------------------------------
+
+(def table
+  [{:cmds [] :fn main :args->opts [:out-file]}])
+
+(defn -main [& args]
+  (cli/dispatch table args))
+
+(when (= *file* (System/getProperty "babashka.file"))
+  (apply -main *command-line-args*))
