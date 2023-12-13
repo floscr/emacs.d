93c100387e3ffec4a678920da716bf30527d1a60
Florian Schroedl <flo.schroedl@gmail.com>
2023-12-13 08:20:57 +0100

    Add nix template

---
+++ project-templates/nix/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "__PROJECT-NAME__";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.flake-compat = {
+    url = "github:edolstra/flake-compat";
+    flake = false;
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config = { allowUnfree = true; };
+        };
+      in
+      {
+        devShell = pkgs.mkShell rec {
+          buildInputs = with pkgs; [
+          ];
+          shellHook = ''
+          '';
+        };
+      });
+}
